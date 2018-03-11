f5496d1a2c63989828b4f257b2b156f26c1cbe4e
sergentum <sergentums@gmail.com>
2018-03-11 17:15:23 +0700

    sdip etm ms

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 inputFile = open("dip_state.txt", "r")
 array = inputFile.readlines()
 findhead = 0
-SDIPname = ""
+SDIP = ""
+SNT = ""
+MS = ""
 for s in array:
+    # work with string after header
     if findhead == 1:
-        findhead =0
-        SDIPname = s[0:5]
-    if s.find("SDIP") == 0:
-        print("SDIP found")
+        findhead = 0
+        SDIP = s[0:8]
+        SNT = s[9:22]
+
+    # find header and mark it
+    if s.find("SDIP     SNT") != -1:
         findhead = 1
 
-    print(SDIPname)
+    # find MS-* in
+    if s.find("SDIP     SNT") == -1:
+        if s.find("MS") != -1:
+            MS = s[22:28]
+
+    # print(s.find("MS-"))
+
+    print(SDIP + SNT + MS)
     # print(s)
 
